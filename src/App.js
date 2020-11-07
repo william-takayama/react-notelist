0a62581948bc0cc83238a3be1eb177b535847db0
William S. Takayama <60292132+william-takayama@users.noreply.github.com>
2020-11-07 17:35:11 -0300

    (Feat) Basic variables

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Form from './components/Form';
+import NoteList from './components/NoteList';
+import classes from './assets/App.module.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/**
+ * react -libray
+ * React - ecossystem
+ */
+
+class App extends Component {
+  render() {
+    return (
+      <section className={classes.content}>
+        <Form />
+        <NoteList />
+      </section>
+    );
+  }
 }
 
 export default App;
