287496e719301ed45a39c394c07d75a21dba6cf5
William S. Takayama <60292132+william-takayama@users.noreply.github.com>
2020-11-21 16:16:55 -0300

    (Feat) Delete note

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Form from './components/Form';
 import NoteList from './components/NoteList';
+import CategoryList from './components/CategoryList';
 import classes from './assets/App.module.scss';
 
 /**
@@ -25,11 +26,25 @@ class App extends Component {
     this.setState(newState);
   }
 
+  deleteNote(index) {
+    let arrayOfNotes = this.state.notes;
+    arrayOfNotes.splice(index, 1);
+    this.setState({
+      notes: arrayOfNotes,
+    });
+  }
+
   render() {
     return (
       <section className={classes.content}>
         <Form createNote={this.createNote.bind(this)} />
-        <NoteList notes={this.state.notes} />
+        <main className={classes.mainCategories}>
+          <CategoryList />
+          <NoteList
+            notes={this.state.notes}
+            deleteNote={this.deleteNote.bind(this)}
+          />
+        </main>
       </section>
     );
   }
